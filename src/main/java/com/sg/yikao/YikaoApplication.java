a35782e12f76706609cfbade18d1c90adc627d70
ShGuan <305666742@qq.com>
2019-03-12 23:18:37 +0800

    github同步一下

---
+++ src/main/java/com/sg/yikao/YikaoApplication.java
@@ -0,0 +1,13 @@
+package com.sg.yikao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YikaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YikaoApplication.class, args);
+	}
+
+}
