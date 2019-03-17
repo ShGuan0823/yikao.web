d9867040fa2a9b74c2bc0b3dec9abae112d2c609
ShGuan <305666742@qq.com>
2019-03-17 22:30:14 +0800

    实现注册失败，保留表单内容

--- src/main/java/com/sg/yikao/controller/RegisterController.java
+++ src/main/java/com/sg/yikao/controller/RegisterController.java
@@ -5,10 +5,11 @@ import com.sg.yikao.entity.User;
 import com.sg.yikao.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import org.apache.commons.lang3.StringUtils;
 
 
 import javax.servlet.http.HttpSession;
@@ -30,19 +31,29 @@ public class RegisterController {
     private RegisterService registerService;
 
     @GetMapping("/register")
-    public String reigister(){
-        return "signup";
+    public ModelAndView reigister(@ModelAttribute("userForm")User user){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("signup");
+        return view;
     }
 
     @PostMapping("/toregister")
-    public String registerUser(User user, @RequestParam("captcha") String captcha, HttpSession session) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-
-        if (!captcha.equals(session.getAttribute(Constants.KAPTCHA_SESSION_KEY))){
-            return "redirect:register";
+    public RedirectView registerUser(User user, @RequestParam("captcha") String captcha, HttpSession session, RedirectAttributes attributes)
+            throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        RedirectView view = new RedirectView();
+        // 判空
+        if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword()) || StringUtils.isBlank(user.getMail())){
+            attributes.addFlashAttribute("userForm", user);
+            view.setUrl("/yikao/register");
+        }else {
+            registerService.RegisterUser(user);
+            view.setUrl("/yikao/login");
         }
+//        if (!captcha.equals(session.getAttribute(Constants.KAPTCHA_SESSION_KEY))){
+//            view.setUrl("/register");
+//        }
 
-        registerService.RegisterUser(user);
-        return "redirect:login";
+        return view;
 
     }
 
